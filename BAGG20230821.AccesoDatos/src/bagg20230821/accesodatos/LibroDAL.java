e9f1f701f1f3738e2e06c9dcdd8f5152f1b89c66
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-08-21 10:04:22 -0600

    Avances 10:00

---
+++ BAGG20230821.AccesoDatos/src/bagg20230821/accesodatos/LibroDAL.java
@@ -0,0 +1,14 @@
+package bagg20230821.accesodatos;
+
+import bagg20230821.entidadesdenegocio.Libro;
+import java.util.*;
+import java.sql.*;
+
+public class LibroDAL {
+
+static String getFields(){
+    return "r.Id, r.Nombre";
+}
+
+    
+}
